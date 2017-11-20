03105f6b0faf09265228ffc921ddc951ae91df8c
Brynner Brito <brynner.qualitec@gmail.com>
2017-11-20 20:07:23 -0300

    Criar a função Bubble Sort e utilizar a função Pop Back

--- main.cpp
+++ main.cpp
@@ -10,41 +10,72 @@ bool trocar ( int &, int & );
 bool trocar ( double &, double & );
 void outputVector( const vector< int > & );
 void funcArray( vector< int > & );
+void bubbleSort( vector< int > & );
 
 
 int main()
 {
+    
     int arraySize = 0;
-    int count = 0;    
-    const int NITER = 5;
+    
     
     cout << "Entre com o tamanho do Array: " << '\n';
     cin >> arraySize;
     vector< int > vetorFinal ( arraySize);
     
-    funcArray( vetorFinal );
+       
+   
+    funcArray( vetorFinal );   
+    
     
     cout << "Os valores do Array sao: " << '\n';
-    for (int i = 0; i < arraySize; i++)
+    for (int i = 0; i < vetorFinal.size(); i++)
         cout << vetorFinal[i] << '\n';
+    
+    
+    bubbleSort(vetorFinal);
+    
+    
+    cout << "Os novos valores do Array sao: " << '\n';
+    for (int i = 0; i < vetorFinal.size(); i++)
+    cout << vetorFinal[i] << '\n';
+    
+    
+    while(!vetorFinal.empty())
+    {
+        vetorFinal.pop_back();
+        
+         for (int i = 0; i < vetorFinal.size(); i++)
+            cout << vetorFinal[i] << '\t';
+            
+         cout << '\n';
+        
+    }
+    
+    
+    system ("pause"); 
+    return 0;
+
+}
+
+
+void bubbleSort( vector< int > & vetorFinal)
+{
+    const int NITER = 5;
+    int count = 0; 
     
     for (int i = 0; i < NITER - 1; i++)
     {
-        for(int j = 1; j < arraySize; j++)
+        for(int j = 1; j < vetorFinal.size(); j++)
         {
             trocar( vetorFinal[j], vetorFinal[j-1] );
             count++;
         }
     }
         
-    cout << "Os novos valores do Array sao: " << '\n';
-    for (int i = 0; i < arraySize; i++)
-        cout << vetorFinal[i] << '\n';
-    
     cout << "Total de trocas: " << count << '\n';
     
-    system ("pause"); 
-    return 0;
+
 }
 
 bool trocar ( int & x, int & y )
